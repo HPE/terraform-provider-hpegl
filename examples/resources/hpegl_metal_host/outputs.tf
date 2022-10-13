3dcbe028b97a2710225563ef0bac604265580d4a
J0HNB0Y <johnny.glynn@hpe.com>
2022-10-13 14:43:04 +0100

    Updated metal documentation for version 1.3.28

--- examples/resources/hpegl_metal_host/outputs.tf
+++ examples/resources/hpegl_metal_host/outputs.tf
@@ -1,4 +1,22 @@
 output "ips" {
-  # Output a map of hostame with all the IP addresses assigned on each network.
+  # Output a map of hostname with each network's IP address.
   value = zipmap(hpegl_metal_host.terra_host.*.name, hpegl_metal_host.terra_host.*.connections)
 }
+
+output "subnets" {
+  # Output a map of hostname with each network's subnet address.
+  value = zipmap(hpegl_metal_host.terra_host.*.name, hpegl_metal_host.terra_host.*.connections_subnet)
+}
+
+output "gateways" {
+  # Output a map of hostname with each network's gateway address.
+  value = zipmap(hpegl_metal_host.terra_host.*.name, hpegl_metal_host.terra_host.*.connections_gateway)
+}
+
+output "vlans" {
+  # Output a map of hostname with each network's vlan.
+  value = zipmap(hpegl_metal_host.terra_host.*.name, hpegl_metal_host.terra_host.*.connections_vlan)
+}
+
+
+
