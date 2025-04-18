641cf1a25ae86ab1322cb95058cb6c30a64b0a03
Anthony Moore <100708401+AnthonyMooreHPE@users.noreply.github.com>
2025-04-18 11:15:35 +0100

    Running 'make docs-generate' ahead of release (#322)

--- examples/data-sources/hpegl_vmaas_instance_disk_type/data-source.tf
+++ examples/data-sources/hpegl_vmaas_instance_disk_type/data-source.tf
@@ -1,7 +1,8 @@
-# (C) Copyright 2024 Hewlett Packard Enterprise Development LP
+# (C) Copyright 2024-2025 Hewlett Packard Enterprise Development LP
 
 data "hpegl_vmaas_instance_disk_type" "vmaas_cloud_vmware_thin_lazy" {
   name      = "thick (lazy zero)"
   cloud_id  = data.hpegl_vmaas_cloud.cloud.id
+  group_id  = data.hpegl_vmaas_group.default_group.id
   layout_id = data.hpegl_vmaas_layout.vmware.id
 }
